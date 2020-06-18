88a96be333afb018f96dcf0679b95b3869eaf8be
Ahmed Elsaid <59203247+AhmedMuhammedElsaid@users.noreply.github.com>
2020-06-18 08:23:34 +0200

    Update actions.js

--- src/Store/actions.js
+++ src/Store/actions.js
@@ -1,11 +1,10 @@
 import actionTypes from "../Types";
 import { OrderData } from "../Logic";
 
-let url ="https://.api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40";
 
 export const fetchWeatherData = () => {
   return (dispatch) => {
-    fetch(url)
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40')
       .then((res) => {
         const data = res.json();
         return data;
